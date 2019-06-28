7bf3a9bb6114bc93006fa9cf4d48ddbb7f34e8cd
Ilya <iiv@checku.co>
2019-06-28 16:53:36 +0300

    fixed radius view

--- index.js
+++ index.js
@@ -1,43 +1,69 @@
+const startRadiusMap = {
+  l: {t: 50, b:50},
+  t: {l: 50, r:50},
+  r: {t: 50, b:50},
+  b: {l: 50, r:50}
+}
+let sideSize;
+
+function countRadiusValue(clientZ, offset) {
+  let val = (clientZ- offset) * 100 / sideSize;
+  val = val < 0 ? 0 : val > 100 ? 100 : val;
+  return parseInt(val)
+}
+
+function redrawFigure(r,) {
+  let borderRadiusValue = redrawBorderRadius(r);
+  let text = `border-radius: ${borderRadiusValue};`
+  changeRadiusInfo(r, text);
+}
+
+function redrawBorderRadius(r) {
+  let radius_str = `
+    ${r.t.l}% ${r.t.r}% ${r.b.r}% ${r.b.l}%
+    \/
+    ${r.l.b}% ${r.r.t}% ${r.r.b}% ${r.l.t}%`
+  document.getElementById('figure').style.borderRadius = radius_str;
+  return radius_str
+}
+
+function changeRadiusInfo(r, text) {
+  document.getElementById('radius_info').innerHTML = text;
+}
+
 function listenSpans() {
   let spans = document.getElementsByTagName('SPAN');
 
-  let l = 50;
-  let t = 50;
-  let r = 50;
-  let b = 50;
+  let r = Object.assign({}, startRadiusMap);
 
   for (let item of spans) {
-    let parent = item.parentElement;
-    let val = null;
 
     function roll(e) {
       e.preventDefault();
       if (item.id == 'left' || item.id == 'right') {
-        // x = e.clientX - parent.offsetLeft;
-        val = (e.clientY - parent.offsetTop) * 100 / 300;
-        val = val < 0 ? 0 : val > 100 ? 100 : val;
+        let val = countRadiusValue(e.clientY, item.parentElement.offsetTop)
         item.style.top = `calc(${val}% - 2.5%)`;
         if (item.id == 'left') {
-          l = val;
+          r.l = {b: val, t: 100-val};
         } else {
-          r = 100 - val;
+          r.r = {b: 100-val, t: val};
         }
       } else {
-        val = (e.clientX - parent.offsetLeft) * 100 / 300;
-        val = val < 0 ? 0 : val > 100 ? 100 : val;
+        let val = countRadiusValue(e.clientX, item.parentElement.offsetLeft)
         item.style.left = `calc(${val}% - 2.5%)`;
         if (item.id == 'top') {
-          t = 100 - val;
+          r.t = {l: val, r: 100 - val};
         } else {
-          b = val;
+          r.b = {l: val, r: 100 - val};
         }
       }
-      document.getElementById('figure').style.borderRadius = `${l}% ${t}% ${r}% ${b}%`;
+      redrawFigure(r);
     }
 
     item.addEventListener('mousedown', () => {
       window.addEventListener('mousemove', roll);
     });
+
     window.addEventListener('mouseup', () => {
       window.removeEventListener('mousemove', roll);
     });
@@ -45,5 +71,10 @@ function listenSpans() {
 }
 
 window.onload = function() {
+  sideSize = 300;
+  let figure = document.getElementById('figure');
+  figure.style.width = `${sideSize}px`;
+  figure.style.height = `${sideSize}px`;
+  redrawFigure(startRadiusMap);
   listenSpans();
 };
